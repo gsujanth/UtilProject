b8afcc6dc8f0148ac22fe7beb5ac06351f41d001
gsujanth <princesujanth@gmail.com>
2021-05-25 21:06:58 -0400

    more time handling methods

--- src/main/java/com/sujanth/utils/dateutils/DateUtil.java
+++ src/main/java/com/sujanth/utils/dateutils/DateUtil.java
@@ -1,7 +1,11 @@
 package com.sujanth.utils.dateutils;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
@@ -14,7 +18,7 @@ public class DateUtil {
 		DateTimeFormatter sourceDateTimeFormatter = new DateTimeFormatterBuilder()
 				.appendPattern("yyyy-MM-dd HH:mm:ss") //24-hour format
 				.optionalStart()
-				.appendFraction(ChronoField.MICRO_OF_SECOND,1,6,true)
+				.appendFraction(ChronoField.MICRO_OF_SECOND,1,6,true) //treats microseconds as optional
 				.optionalEnd()
 				.toFormatter();
 		
@@ -30,10 +34,36 @@ public class DateUtil {
 		System.out.println(targetDateTimeFormatter.format(toTargetDateTime));
 		return targetDateTimeFormatter.format(toTargetDateTime);
 	}
+	
+	public static Timestamp convertMilliSecondsToUtcTimestamp (Long milliseconds) {
+		LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(milliseconds, 0, ZoneOffset.UTC);
+		System.out.println(Timestamp.valueOf(localDateTime));
+		return Timestamp.valueOf(localDateTime);
+	}
+	
+	public static long startOfDayInEpochMillis() {
+		return LocalDate.now()
+				.atStartOfDay()
+				.toInstant(ZonedDateTime.now(ZoneId.of("UTC")).getOffset())
+				.toEpochMilli();
+	}
+	
+	public static String dateForTargetTimeZone(String timeZone) {
+		return LocalDate.now(ZoneId.of(timeZone)).toString()+" 00:00:00.00";
+	}
+	
+	public static ZonedDateTime getStartOfTargetTimeZoneDay(String targetTimeZone) {
+		ZonedDateTime startOfTargetTimeZoneDay = LocalDate.now(ZoneId.of(targetTimeZone))
+				.atStartOfDay(ZoneId.of(targetTimeZone));
+		System.out.println(startOfTargetTimeZoneDay);
+		return startOfTargetTimeZoneDay;
+	}
 
 	public static void main(String[] args) {
 		
 		DateUtil.covertBetweenTimezones("2021-05-25 20:10:35.56778", "America/Detroit", "America/Chicago");
+		DateUtil.convertMilliSecondsToUtcTimestamp(Instant.now().toEpochMilli()/1000L);
+		DateUtil.getStartOfTargetTimeZoneDay("America/Detroit");
 
 	}
 
